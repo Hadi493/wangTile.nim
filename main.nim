074be59526a73fe4d6ba564880d85e4cd4ba786a
Hadi493 <hadialam493@gmail.com>
2026-02-20 23:19:22 +0600

    LCFN

--- main.nim
+++ main.nim
@@ -1,9 +1,10 @@
 import system
 import strformat
 import math
+import sequtils
 
-const WIDTH  = 1920
-const HEIGHT = 1080
+const WIDTH  = 512
+const HEIGHT = 512
 
 type RGB = tuple[r: float, g: float, b: float]
 
@@ -17,10 +18,10 @@ proc `/`(a, b: Vec2): Vec2 = (a.x / b.x, a.y / b.y)
 proc length(a: Vec2): float = sqrt(a.x * a.x + a.y * a.y)
 
 proc stripes(uv: Vec2): RGB =
-  let n = 50.0
+  let n = 8.0
   (
     (sin(uv.x * n) + 1.0) * 0.5,
-    (sin((uv.x + uv.y) * n * 10.0) + 1.0) * 0.5,
+    (sin((uv.x + uv.y * (n * n)) * n * 660.0) + 1.0) * 0.5,
     # 0.59,
     (cos(uv.y * n + 0.2) + 1.0) * 0.5
   )
@@ -28,6 +29,12 @@ proc stripes(uv: Vec2): RGB =
 proc circle(uv: Vec2): RGB =
   let a = (vec2(0.5) - uv).length > 0.25
   (1.0, float(a), float(a))
+
+proc wang(uv: Vec2): RGB =
+  let r = 0.5
+  let cs = @[(0.5, 0.0), (0.5, 1.0)]
+  let ps = cs.map(proc(c: Vec2): RGB = (float((cs[0] - uv).length < r), 0.0, 0.0))
+  ps[0]
 
 proc main(): void =
   let f = open("output.ppm", fmWrite)
@@ -38,7 +45,7 @@ proc main(): void =
     for y in 0..<WIDTH:
       let u = float(y) / float(WIDTH)
       let v = float(x) / float(WIDTH)
-      let (r, g, b) = stripes((u, v))
+      let (r, g, b) = wang((u, v))
       f.write(chr(int(r * 255.0)))
       f.write(chr(int(g * 255.0)))
       f.write(chr(int(b * 255.0)))
